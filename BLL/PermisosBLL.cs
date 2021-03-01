754e2bce2a696269e6ec149941b5be5288b2e99c
Erickson Fana Morales <erickson_fana@ucne.edu.do>
2021-03-01 14:24:02 -0400

    Add project files.

---
+++ BLL/PermisosBLL.cs
@@ -0,0 +1,75 @@
+﻿using Registro_Detalle.DAL;
+using Registro_Detalle.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Registro_Detalle.BLL
+{
+    public class PermisosBLL
+    {
+        public static Permisos Buscar(int id)
+        {
+            Contexto contexto = new Contexto();
+            Permisos permisos;
+
+            try
+            {
+                permisos = contexto.Permisos.Find(id);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+
+            return permisos;
+        }
+
+        public static List<Permisos> GetList(Expression<Func<Permisos, bool>> criterio)
+        {
+            List<Permisos> lista = new List<Permisos>();
+            Contexto contexto = new Contexto();
+            try
+            {
+                lista = contexto.Permisos.Where(criterio).ToList();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+
+            return lista;
+        }
+
+        public static List<Permisos> GetPermisos()
+        {
+            List<Permisos> lista = new List<Permisos>();
+            Contexto contexto = new Contexto();
+            try
+            {
+                lista = contexto.Permisos.ToList();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+
+            return lista;
+        }
+    }
+}
