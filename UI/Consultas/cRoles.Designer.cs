47a8c132880d407a644ec73e9c9bc4574c5186eb
Erickson Fana Morales <erickson_fana@ucne.edu.do>
2021-03-15 16:40:35 -0400

    Agregando cambios

--- UI/Consultas/cRoles.Designer.cs
+++ UI/Consultas/cRoles.Designer.cs
@@ -29,61 +29,219 @@ namespace Registro_Detalle.UI.Consultas
         /// </summary>
         private void InitializeComponent()
         {
-            this.checkBox1 = new System.Windows.Forms.CheckBox();
-            this.label1 = new System.Windows.Forms.Label();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(cRoles));
+            this.Buscarbutton = new System.Windows.Forms.Button();
+            this.EsActivo = new System.Windows.Forms.GroupBox();
+            this.InactivosradioButton = new System.Windows.Forms.RadioButton();
+            this.ActivosradioButton = new System.Windows.Forms.RadioButton();
+            this.TodosradioButton = new System.Windows.Forms.RadioButton();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
             this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Imprimirbutton = new System.Windows.Forms.Button();
+            this.EsActivo.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
-            // checkBox1
+            // Buscarbutton
             // 
-            this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(12, 22);
-            this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(134, 19);
-            this.checkBox1.TabIndex = 0;
-            this.checkBox1.Text = "Usar Filtro de Fechas";
-            this.checkBox1.UseVisualStyleBackColor = true;
+            this.Buscarbutton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.Buscarbutton.Image = ((System.Drawing.Image)(resources.GetObject("Buscarbutton.Image")));
+            this.Buscarbutton.Location = new System.Drawing.Point(577, 14);
+            this.Buscarbutton.Name = "Buscarbutton";
+            this.Buscarbutton.Size = new System.Drawing.Size(101, 68);
+            this.Buscarbutton.TabIndex = 15;
+            this.Buscarbutton.Text = "Buscar";
+            this.Buscarbutton.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.Buscarbutton.UseVisualStyleBackColor = true;
             // 
-            // label1
+            // EsActivo
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 63);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(34, 15);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "Filtro";
+            this.EsActivo.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.EsActivo.Controls.Add(this.InactivosradioButton);
+            this.EsActivo.Controls.Add(this.ActivosradioButton);
+            this.EsActivo.Controls.Add(this.TodosradioButton);
+            this.EsActivo.Location = new System.Drawing.Point(470, 7);
+            this.EsActivo.Name = "EsActivo";
+            this.EsActivo.Size = new System.Drawing.Size(96, 74);
+            this.EsActivo.TabIndex = 14;
+            this.EsActivo.TabStop = false;
+            this.EsActivo.Text = "esActivo?";
+            // 
+            // InactivosradioButton
+            // 
+            this.InactivosradioButton.AutoSize = true;
+            this.InactivosradioButton.Location = new System.Drawing.Point(6, 51);
+            this.InactivosradioButton.Name = "InactivosradioButton";
+            this.InactivosradioButton.Size = new System.Drawing.Size(72, 19);
+            this.InactivosradioButton.TabIndex = 2;
+            this.InactivosradioButton.TabStop = true;
+            this.InactivosradioButton.Text = "Inactivos";
+            this.InactivosradioButton.UseVisualStyleBackColor = true;
+            // 
+            // ActivosradioButton
+            // 
+            this.ActivosradioButton.AutoSize = true;
+            this.ActivosradioButton.Location = new System.Drawing.Point(6, 32);
+            this.ActivosradioButton.Name = "ActivosradioButton";
+            this.ActivosradioButton.Size = new System.Drawing.Size(64, 19);
+            this.ActivosradioButton.TabIndex = 1;
+            this.ActivosradioButton.Text = "Activos";
+            this.ActivosradioButton.UseVisualStyleBackColor = true;
+            // 
+            // TodosradioButton
+            // 
+            this.TodosradioButton.AutoSize = true;
+            this.TodosradioButton.Checked = true;
+            this.TodosradioButton.Location = new System.Drawing.Point(8, 13);
+            this.TodosradioButton.Name = "TodosradioButton";
+            this.TodosradioButton.Size = new System.Drawing.Size(56, 19);
+            this.TodosradioButton.TabIndex = 0;
+            this.TodosradioButton.TabStop = true;
+            this.TodosradioButton.Text = "Todos";
+            this.TodosradioButton.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(154, 57);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(302, 23);
+            this.textBox1.TabIndex = 13;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(54, 55);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(85, 23);
+            this.comboBox1.TabIndex = 12;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.dateTimePicker1);
+            this.groupBox1.Controls.Add(this.dateTimePicker2);
+            this.groupBox1.Location = new System.Drawing.Point(154, 7);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(302, 45);
+            this.groupBox1.TabIndex = 11;
+            this.groupBox1.TabStop = false;
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(148, 19);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 15);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Hasta";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 19);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 15);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Desde";
             // 
             // dateTimePicker1
             // 
             this.dateTimePicker1.CustomFormat = "dd/MM/yyyy";
             this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
-            this.dateTimePicker1.Location = new System.Drawing.Point(242, 158);
+            this.dateTimePicker1.Location = new System.Drawing.Point(45, 14);
             this.dateTimePicker1.Name = "dateTimePicker1";
-            this.dateTimePicker1.Size = new System.Drawing.Size(83, 23);
+            this.dateTimePicker1.Size = new System.Drawing.Size(103, 23);
             this.dateTimePicker1.TabIndex = 2;
             // 
             // dateTimePicker2
             // 
+            this.dateTimePicker2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.dateTimePicker2.CustomFormat = "dd/MM/yyyy";
             this.dateTimePicker2.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
-            this.dateTimePicker2.Location = new System.Drawing.Point(518, 177);
+            this.dateTimePicker2.Location = new System.Drawing.Point(185, 14);
             this.dateTimePicker2.Name = "dateTimePicker2";
-            this.dateTimePicker2.Size = new System.Drawing.Size(76, 23);
+            this.dateTimePicker2.Size = new System.Drawing.Size(98, 23);
             this.dateTimePicker2.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(14, 56);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 15);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Filtro";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(14, 23);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(134, 19);
+            this.checkBox1.TabIndex = 9;
+            this.checkBox1.Text = "Usar Filtro de Fechas";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(14, 95);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 25;
+            this.dataGridView1.Size = new System.Drawing.Size(664, 357);
+            this.dataGridView1.TabIndex = 16;
+            // 
+            // Imprimirbutton
+            // 
+            this.Imprimirbutton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.Imprimirbutton.Location = new System.Drawing.Point(14, 458);
+            this.Imprimirbutton.Name = "Imprimirbutton";
+            this.Imprimirbutton.Size = new System.Drawing.Size(75, 32);
+            this.Imprimirbutton.TabIndex = 17;
+            this.Imprimirbutton.Text = "Imprimir";
+            this.Imprimirbutton.UseVisualStyleBackColor = true;
             // 
             // cRoles
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.AppWorkspace;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.dateTimePicker2);
-            this.Controls.Add(this.dateTimePicker1);
+            this.ClientSize = new System.Drawing.Size(693, 496);
+            this.Controls.Add(this.Imprimirbutton);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.Buscarbutton);
+            this.Controls.Add(this.EsActivo);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.checkBox1);
+            this.MinimumSize = new System.Drawing.Size(709, 300);
             this.Name = "cRoles";
-            this.Text = "cRoles";
+            this.Text = "Consulta de Roles";
+            this.EsActivo.ResumeLayout(false);
+            this.EsActivo.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -91,9 +249,21 @@ namespace Registro_Detalle.UI.Consultas
 
         #endregion
 
-        private System.Windows.Forms.CheckBox checkBox1;
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button Buscarbutton;
+        private System.Windows.Forms.GroupBox EsActivo;
+        private System.Windows.Forms.RadioButton InactivosradioButton;
+        private System.Windows.Forms.RadioButton ActivosradioButton;
+        private System.Windows.Forms.RadioButton TodosradioButton;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
         private System.Windows.Forms.DateTimePicker dateTimePicker1;
         private System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button Imprimirbutton;
     }
 }
